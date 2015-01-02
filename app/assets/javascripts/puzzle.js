033e24e558d5a937073cee6de37f218621171832
Allen Blankenship <zuosoft@yahoo.com>
2015-01-02 00:28:35 +0000

    got the pencil working, though the setSquareText -> resize thing is a bit messy

--- app/assets/javascripts/puzzle.js
+++ app/assets/javascripts/puzzle.js
@@ -9,11 +9,12 @@ var resize_ratio = 0.75;
 var resize = function() {
     var resize_size = $('.square-text').parent().width();
     $('.square-text').css('font-size', resize_size * resize_ratio);
+    $('.penciled .square-text').css('font-size', resize_size * resize_ratio * 0.33);
 };
 
 // action bindings (clicks, mouseovers etc)
 function handleSquareMouseEnter ($square) {
-    if ( mouseDown && currentTool.indexOf("highlighter") != -1){
+    if ( mouseDown && textContains(currentTool, "highlighter")){
         highlight($square);
     } else
     if ( mouseDown && currentTool == "eraser") {
@@ -61,22 +62,74 @@ function erase($square, option){
         $square.removeClass("highlighted highlighted-red highlighted-blue");
     } else
     if (option == "onlyHighlight") { return; } else
-    if ($square.hasClass("permanent") === false) {
-        $square.removeClass("penned");
+    if (isChangeable($square)) {
+        clearTooledClasses($square);
         $square.find('.square-text').text("");
     }
 }
 
 function writePen($square) {
-    if ($square.hasClass("permanent") === false && currentNum !== 'none') {
+    if (isChangeable($square) && numSelected()) {
         $square.addClass("penned");
-        $square.find('.square-text').text(currentNum);
+        setSquareText($square, currentNum);
     }
 }
 
 function writePencil ($square) {
-    
+    if (numSelected() && !(isInPen($square) || isPermanent($square)) ) {
+        $square.addClass("penciled");
+        var text = getSquareText($square);
+        var num = currentNum.concat(" ");
+        if (textContains(text, num)){
+            text = text.replace(num, "");
+        } else {
+            text = text.concat(num);
+        }
+        setSquareText($square, text);
+    }
 }
+
+// Helper methods to make the code more readable
+
+function isPermanent($square) {
+    return $square.hasClass("permanent");
+}
+
+function isChangeable($square) {
+    return !isPermanent($square);
+}
+
+function isInPen($square) {
+    return $square.hasClass("penned");
+}
+
+function isInPencil($square) {
+    return $square.hasClass("penciled");
+}
+
+function numSelected() {
+    return currentNum !== "none";
+}
+
+function textContains(string1, string2) {
+    return string1.indexOf(string2) >= 0;
+}
+
+
+function getSquareText($square) {
+    return $square.find('.square-text').text();
+}
+function setSquareText($square, textToSet) {
+    $square.find('.square-text').text(textToSet);
+    resize();
+}
+
+function clearTooledClasses($square) {
+    $square.removeClass('penned');
+    $square.removeClass('penciled');
+}
+
+// The event bindings for the puzzle, clicking, resizing and so forth...
 
 var allBindings = function() { 
     
