420ef94588f35c7bc4ecdcbe82744c75cf4d6ef9
Allen Blankenship <zuosoft@yahoo.com>
2015-02-16 23:04:50 +0000

    Decided not to do a litebox, instead just addded a simple log in/log out button to the menu in the header, also added a route so /signin and /signout do the things they ought to do.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  
 
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
+  get 'signin', to: redirect('auth/facebook')
   
   root 'static_pages#home'
   resources :puzzles, only: [:new, :create, :show, :destroy, :index]
