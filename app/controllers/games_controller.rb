e3aec1453c2b06bbbe71f8c3776f55b7fee69dcd
Allen Blankenship <zuosoft@yahoo.com>
2015-02-23 02:46:17 +0000

    Finished working on the new paginated views for Puzzles, Users and Games the styling is -more- modular, not perfect by a long shot.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,6 +3,7 @@ class GamesController < ApplicationController
   def index
     @user = User.find( params[:users_id] )
     @games = @user.games
+    @games = @games.paginate(page: params[:page], per_page: 5)
   end
   
   def new
